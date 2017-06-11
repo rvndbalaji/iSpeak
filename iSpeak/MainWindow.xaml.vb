8f6968c86fec7844d6a16ee368b8dc97439c3b31
Aravind Balaji <aravind.balaji@live.com>
2017-06-12 01:29:12 +0530

    Uploaded

---
+++ iSpeak/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿
+
+Class MainWindow
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Button1.Click
+
+        Dim speech = CreateObject("sapi.spvoice")
+        speech.speak(text.Text)
+
+    End Sub
+End Class
